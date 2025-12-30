3e32ed9b43e6722a85b62bc615c05b0cf3ddc235
manishkumarmdb <manishkumarmdb@gmail.com>
2025-12-30 15:43:52 -0600

    practiced to read input from console

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-print('Hellow world!')
+### This is a simple Python script that prints "Hello world!" to the console.
+print('Hello world!')
+
+### Version that asks for user input
+name = input('Enter your name: ')
+print('Hello')
+print(name)
+
+### Improved version to be call in a single line
+print(f'Hello, {name}!')
