859120b2c4bd4ac38cc4f73c5acd629234ebd6d3
manishkumarmdb <manishkumarmdb@gmail.com>
2025-12-31 01:25:47 -0600

    practiced more on string operations

--- string.py
+++ string.py
@@ -1,4 +1,17 @@
 ### This script demonstrates various string operations in Python.
+first_name = 'Setu'
+last_Name = 'Choudhari'
+print('Hello, ' + first_name + ' ' + last_Name + '!')
+print('Hello, {} {}!'.format(first_name, last_Name))
+### Using positional arguments in format method
+print('Hello, {0} {1}!'.format(first_name, last_Name))
+print('Hello, {1} {0}!'.format(first_name, last_Name))
+### Using f-strings for string interpolation
+print(f'Hello, {first_name} {last_Name}!')
+### Storing the full greeting in a variable
+full_name = f'Hello, {first_name} {last_Name}!'
+
+#### Taking user input for first and last name
 first_name = input('Enter your first name: ')
 last_Name = input('Enter your last name: ')
 full_name = first_name + ' ' + last_Name
