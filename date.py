86a71436ee46eed12e40d09e40cc4b198f0e26ac
manishkumarmdb <manishkumarmdb@gmail.com>
2026-01-02 19:25:50 -0600

    practiced more on date operations

--- date.py
+++ date.py
@@ -6,3 +6,10 @@ print("Current Date and Time:", current_date)
 one_day = timedelta(days=1)
 yesterday = current_date - one_day
 print("Yesterday's Date and Time:", yesterday)
+
+print('Day:', current_date.day)
+print('Month:', current_date.month)
+print('Year:', current_date.year)
+print('Hour:', current_date.hour)
+print('Minute:', current_date.minute)
+print('Second:', current_date.second)
