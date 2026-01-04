700b03b5d547c17497beca0798437ee6283b13c0
manishkumarmdb <manishkumarmdb@gmail.com>
2026-01-04 09:15:01 -0600

    practiced more on date operations

--- date.py
+++ date.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 current_date = datetime.now()
-print("Current Date and Time:", current_date)
+print('Current Date and Time: ', current_date)
 
 one_day = timedelta(days=1)
 yesterday = current_date - one_day
@@ -14,8 +14,11 @@ print('Hour:', current_date.hour)
 print('Minute:', current_date.minute)
 print('Second:', current_date.second)
 
+# Taking user input for birthday
 birthday = input('Enter your birthday (YYYY-MM-DD): ')
+# Converting string input to datetime object
 birthday_date = datetime.strptime(birthday, '%Y-%m-%d')
 print('Your birthday is on:', birthday_date)
+# Calculating next birthday date
 next_birthday = birthday_date.replace(year=current_date.year)
 print('This year, your birthday will be on:', next_birthday)
