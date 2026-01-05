968273c84f76a59db063c23c6e3b66313b5b728f
manishkumarmdb <manishkumarmdb@gmail.com>
2026-01-04 23:22:10 -0600

    practiced on error handling

---
+++ error_handling.py
@@ -0,0 +1,11 @@
+# Simple comparison between two numbers
+x = 10
+y = 12
+if y > x :
+    print("y is greater than x")
+if x < y :
+    print(str(x) + " is less than " + str(y))
+
+# Handling division by zero error
+b = 0
+print(x / b)
